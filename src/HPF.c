e17fa1565fcd36b5810b278557adbc2f870cad1e
Pillager225 <ryanloringcooper@gmail.com>
2018-04-19 16:50:56 -0700

    removed invalid HPF code that caused a lot of errors

--- src/HPF.c
+++ src/HPF.c
@@ -3,25 +3,7 @@
 #include <string.h>
 #include "HPF.h"
 
-void giveQuantaToProc(ProcInfo *proc, int curTime) {
-    if(proc->completedRunTime == 0) {
-        proc->totalWaitTime += curTime-proc->arrivalTime;
-        proc->startTime = curTime;
-        proc->responseTime = proc->totalWaitTime;
-    }
-    proc->completedRunTime++;
-}
-
 void doHPF(ProcInfo *procs, int numProcs, int preemptive) {
-	RunInfo *ri;
-	float timeLeft;
-    bool done = false;
-	ProcInfo *curRun = NULL, *procCopy = (ProcInfo *)malloc(numProcs*sizeof(ProcInfo));
-	ProcInfo **finished = (ProcInfo**)calloc(numProcs,sizeof(ProcInfo*));
-	int finishedIndex = 0, nextProc = 0, curTime = 0;
-	PriorityQueue *pq = initializePriorityQueue(NUM_PRIORITIES);	
-
-	memcpy(procCopy, procs, numProcs*sizeof(ProcInfo));
 
 	printf("Starting highest priority first algorithm");
     if(preemptive) {
@@ -31,37 +13,4 @@ void doHPF(ProcInfo *procs, int numProcs, int preemptive) {
     }
 	printProcs(procs, numProcs, stdout);
     
-    while(!done) {
-        curTime++;
-        if(curTime < desiredQuanta && nextProc < numProcs && procCopy[nextProc].arrivalTime <= curTime) {
-           addProc(&pq, &procCopy[nextProc++]); 
-        }
-        if(curRun == NULL) {
-            if((curRun = getNextProc(&pq)) == NULL) {
-                done = true;
-                break;
-            }
-        }
-        if(curRun != NULL) {
-            giveQuantaToProc(curRun, curTime);
-            if(curRun->completedRunTime > curRun->totalRunTime) {
-                finished[finishedIndex++] = curRun;
-                curRun = NULL;
-            }
-        } else {
-            fprintf(stderr, "Processor was unused during %d quantum\n", curTime);
-        }
-    }
-
-	while((temp = getNextProc(&pq)) != NULL) {
-   // TODO 
-	}
-
-    printResults(finished, finishedIndex, ri->timeChart, ri->iters, numProcs, ri->runTime);
-//  note that finished[i] doesn't have to be freed because it points to a part of procCopy
-    free(finished);
-	cleanupPriorityQueue(&pq);
-	pq = NULL;
-	free(procCopy);
-	procCopy = NULL;
 }
